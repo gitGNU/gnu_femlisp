055932dd89536a5c00e0f139fc688b71e234b00e
Nicolas Neuss <neuss@scipolis.de>
2017-01-16 15:11:49 +0100

    Plotting switched off depending on DISPLAY

--- src/finalize.lisp
+++ src/finalize.lisp
@@ -34,6 +34,44 @@
 
 (in-package :fl.start)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Register banner
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun femlisp-version () *femlisp-version*)
+(defun femlisp-herald () (format nil "    Femlisp/~a" (femlisp-version)))
+
+(defun femlisp-banner ()
+  (format
+   t "~&~%*** Femlisp-~A ***
+
+Copyright (C) 2003-2017
+Nicolas Neuss
+University Heidelberg, KIT Karlsruhe, University Erlangen-Nuernberg.
+
+Femlisp comes with ABSOLUTELY NO WARRANTY, for details see the
+file LICENSE in the Femlisp main directory.  This is free
+software, and you are welcome to redistribute it under certain
+conditions.
+
+You can enter \"(fl.demo:femlisp-demo)\" to get a guided tour
+through Femlisp, and enter \"(quit)\" to leave the program.~%~%"
+   *femlisp-version*))
+
+(defun femlisp-restart ()
+  "Should be called when restarting a saved Femlisp core."
+  #+(or cmu scl) (progn (ext::print-herald))
+  (femlisp-banner)
+  (setq *read-default-float-format* 'double-float)
+  (setq *package* (find-package :fl.application))
+  #+linux
+  (unless (fl.port:getenv "DISPLAY")
+    (warn "No DISPLAY variable set which is necessary for running dx.~%~
+Therefore, interactive graphics is switched off.~%~
+You may switch it on again using: (setq fl.port::*plot* T)")
+    (setq fl.plot::*plot* nil))
+  )
+
 (pushnew :femlisp *features*)
 
 (femlisp-banner)
