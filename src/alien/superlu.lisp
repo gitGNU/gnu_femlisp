8f509af53c42fa3d0f6e555c98d00cb06dce1c48
Nicolas Neuss <neuss@scipolis.de>
2017-03-29 21:12:16 +0200

    Correct calling superlu

--- src/alien/superlu.lisp
+++ src/alien/superlu.lisp
@@ -70,9 +70,8 @@
 (defun superlu (m n nnz cs ri store nrhs rhs sol orientation)
   "Calls SuperLU."
   (fl.port:foreign-call
-   #'c-superlu m n nnz (vector-sap cs) (vector-sap ri)
-   (vector-sap store) nrhs (vector-sap rhs)
-   (vector-sap sol)
+   #'c-superlu m n nnz cs ri
+   store nrhs rhs sol
    orientation))
 
 ;;; Testing
