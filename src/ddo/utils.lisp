5fff381d76939e7bb0b8d4497f36928561d59d61
Nicolas Neuss <neuss@scipolis.de>
2017-03-29 21:29:04 +0200

    Corrections and better debugging support

--- src/ddo/utils.lisp
+++ src/ddo/utils.lisp
@@ -35,14 +35,34 @@
 
 (in-package :ddo)
 
+(defun mpi-rank ()
+  (when (mpi-initialized)
+    (mpi-comm-rank)))
+
 (defun mpi-dbg (id format-string &rest args)
   (let ((rank (mpi-comm-rank)))
     (when (or (eq *report-ranks* T)
               (member rank *report-ranks*))
       (apply #'dbg id
-             (format nil "Rank ~D:~A" rank format-string)
+             (format nil "Rank ~3D:~A" rank format-string)
              args))))
 
+(defun mpi-debug-file (&optional rank)
+  (parse-namestring
+   (format nil "femlisp:data;mpilog-~3,'0D"
+           (or rank (mpi-comm-rank)))))
+
+(defmacro with-split-mpi-debug-output (&body body)
+  (with-gensyms (stream)
+    `(with-open-file (,stream (mpi-debug-file)
+                              :direction :output
+                              :if-exists :append
+                              :if-does-not-exist :create)
+       (let ((fl.debug::*debug-io* ,stream))
+         (format fl.debug::*debug-io* "~&*** Rank ~3D ***~%" (mpi-comm-rank))
+         (force-output fl.debug::*debug-io*)
+         ,@body))))
+  
 (defmacro track-time ((var) &body body)
   (with-gensyms (timespan result)
     `(lret (,result)
@@ -54,9 +74,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  Helper functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun all-processors ()
-  (loop for k below (mpi-comm-size) collect k))
 
 (defun neighbors-for (object-or-local-id)
   "All processors for a distributed object"
@@ -76,12 +93,24 @@ whole DDO concept, so that its use should be avoided whenever possible."
   (apply #'< (mpi-comm-rank) (neighbors-for object-or-local-id)))
 
 (defun neighbors ()
-  "Default and simplest: all processors without me"
-  (remove (mpi-comm-rank) (range< 0 (mpi-comm-size))))
+  "Look with which processors we are connected."
+  (let ((rank (mpi-comm-rank)))
+    (accessing-exclusively ((distribution *distribution*))
+      (loop for proc below (mpi-comm-size)
+            unless (or (= rank (mpi-comm-rank))
+                       (not (R-select distribution '_ proc '_)))
+              collect proc))))
 
 (defmacro do-neighbors ((proc) &body body)
   `(loop for ,proc in (neighbors)
          do ,@body))
+
+(defmacro do-processors ((proc) &body body)
+  `(loop for ,proc below (mpi-comm-size)
+         do ,@body))
+
+(defun all-processors ()
+  (loop for k below (mpi-comm-size) collect k))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  For profiling relation handling
@@ -195,8 +224,122 @@ communication, needs on the currently active distributed objects."
                       (apply #'mpi-irecv-anything proc :tag tag
                              (and decode (list :decode decode))))))))))
 
+(defun unsymmetric-at (array i j)
+  (/= (aref array i j)
+      (aref array j i)))
+
+(defun array-symmetric-p (array)
+  (let ((dims (array-dimensions array)))
+    (assert (samep dims) () "Array is non-quadratic")
+    (let ((n (first dims)))
+      (dotimes (i n)
+        (dotimes (j i)
+          (when (unsymmetric-at array i j)
+            (return-from array-symmetric-p (values nil i j))))))
+    t))
+
+(defun connection-string (connections)
+  "Prints a connection array in compact quadratic form."
+  (with-output-to-string (stream)
+    (loop for i below (array-dimension connections 0) do
+      (format stream "~{~2A~}~%"
+              (loop for j below (array-dimension connections 1)
+                    collect
+                    (ecase (aref connections i j)
+                      (0 0)
+                      (1 (if (= (aref connections j i) 1)
+                             1
+                             '*))))))))
+
+(defun distribution-consistency-check ()
+  "Does a communication between all processors and checks if the distribution fits.
+The result should be a vector containing only zeros."
+  (accessing-exclusively ((distribution *distribution*)
+                          (distributed-objects *distributed-objects*))
+    (let ((sharing (map +ulong-vec+
+                        (lambda (proc)
+                          (if (R-select distribution '_ proc '_) 1 0))
+                        (all-processors))))
+      (let* ((comm-data
+               (loop for proc in (all-processors)
+                     collect (list :send proc sharing)
+                     collect (list :receive proc)))
+             (comm-result (exchange comm-data :tag 0))
+             (my-rank (mpi-comm-rank)))
+        (let* ((items (cons (list my-rank nil sharing)
+                            comm-result))
+               (connections (make-array (twice (mpi-comm-size)))))
+          ;; fill with content
+          (loop for (proc nil data) in items do
+            (do-processors (k)
+              (setf (aref connections proc k)
+                    (aref data k))))
+          (let ((consistent-p (array-symmetric-p connections)))
+            (unless consistent-p
+              (mpi-dbg :communication "Inconsistent connections:~%~A"
+                       (connection-string connections))
+              ;; Report own inconsistencies if there should be any
+              (do-processors (proc)
+                (when (and (unsymmetric-at connections my-rank proc)
+                           (eql (aref connections my-rank proc) 1))
+                  (mpi-dbg :communication "~&For processor ~D:~%" proc)
+                  (loop for (local-id nil nil)
+                          in (R-select distribution '_ proc '_)
+                        do
+                           (mpi-dbg :communication "~A~%"
+                                    (gethash local-id distributed-objects))))))
+            (values consistent-p connections)))))))
+
+;;; Debugging
+
+(defun analyze-mpi-debug-output ()
+  "Analyzes send/receive output.  This is probably done better prior
+to synchronization by distribution-consistency-check."
+  (let* ((np (lfarm:kernel-worker-count))
+         (outputs
+           (loop for rank below np collect
+             (with-open-file (stream (ddo::mpi-debug-file rank))
+               (loop for line = (read-line stream nil nil)
+                     while line collect line)))))
+    (assert (samep (mapcar #'length outputs)))
+    (let ((nr-comms (length (first outputs))))
+      (let ((send-pattern (make-array (list nr-comms np np) :initial-element nil))
+            (receive-pattern (make-array (list nr-comms np np) :initial-element nil)))
+        (loop for comm below nr-comms do
+          (loop for output in outputs
+                and rank from 0
+                for line = (nth comm output)
+                when (search "SEND=" line) do
+                  (let ((sends (+ 5 (search "SEND=" line)))
+                        (receives (+ 8 (search "RECEIVE=" line))))
+                    (loop for k below np do
+                      (when (eql (aref line (+ k sends)) #\1)
+                        (setf (aref send-pattern comm rank k) T))
+                      (when (eql (aref line (+ k receives)) #\1)
+                        (setf (aref receive-pattern comm rank k) T))))))
+        ;; check that they are transposed in the latter variables
+        (loop with flag = nil
+              for comm below nr-comms
+              do
+                (loop for i below np do
+                  (loop for j below np do
+                    (unless (eql (aref send-pattern comm i j)
+                                 (aref receive-pattern comm j i))
+                      (format t "Problem at comm=~D: (~D ~A-~A ~D)~%"
+                              comm i
+                              (if (aref send-pattern comm i j) #\> #\|)
+                              (if (aref receive-pattern comm j i) #\> #\|)
+                              j)
+                      (setf flag t))))
+                (when flag
+                  (return (list (lambda (i j) (aref send-pattern comm i j))
+                                (lambda (i j) (aref receive-pattern comm i j))))))))))
+
 
 (defun test-utils ()
   (ddo-performance-check)
+  (make-array '(2 2) :initial-contents '(#(1 2) #(3 4)))
+  (connection-string #2a((0 1) (0 0)))
+  (array-symmetric-p #2a((0 0) (0 0)))
   )
 
